5cffa39acd1c0975706ddc63ac02a3dab39a017a
Ian Schwartz <ian.schwartz23@gmail.com>
2019-10-29 15:46:48 -0700

    PUT

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -71,6 +71,20 @@ router.post('/:id/comments', (req, res) => {
 });
 
 // PUT to /api/posts/:id
+router.put('/:id', (req, res) => {
+    if (!req.body.title || !req.body.contents) {
+      res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    } else {
+        db.update(req.params.id, req.body).then(post => {
+          if (post) {
+            res.status(200).json(post);
+          } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." });
+          }
+        })
+        .catch(() => res.status(500).json({ error: "The post information could not be modified." }));
+    }
+});
 
 
 // DELETE to /api/posts/:id
